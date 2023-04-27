0c32e9bb09420201ac3da7ce5972503f4c0e0701
hydroponka <Ceageenk02@gmail.com>
2023-04-27 22:57:06 +0300

    1

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (org.apache.commons.lang3.StringUtils.isNumeric(str)){
+            int num = Integer.parseInt(str);return num > 0;
+        } else {
+            return false;
+        }
          //here magic will happen
     }
 }
